34ac9009f1e66930d0259a34e97b9bf1cf9f2ce1
hitman1439 <37571052+hitman1439@users.noreply.github.com>
2020-08-03 00:17:37 +0900

    파일업로드 기능 추가

--- src/main/java/com/isaac/bcu/file/FileDao.java
+++ src/main/java/com/isaac/bcu/file/FileDao.java
@@ -1,10 +1,14 @@
 package com.isaac.bcu.file;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
-
+import org.springframework.util.FileCopyUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.isaac.bcu.homework.HomeworkVO;
 
@@ -14,6 +18,10 @@ public class FileDao{
 	@Autowired
 	SqlSession sqlSession;
 
+	@Autowired
+	@Qualifier("uploadPath")
+	String uploadPath;
+
 	public List<HomeworkVO> list(HomeworkVO hwVO){
 		return sqlSession.selectList("_homework.list", hwVO);
 	}
@@ -22,7 +30,33 @@ public class FileDao{
 		return sqlSession.selectOne("_homework.view", hwVO);
 	}
 
-	public int insert(FileVO fileVO) {
-		return sqlSession.insert("_file.insert", fileVO);
+	public int insert(MultipartFile mfile) throws IOException {
+
+		FileVO fvo = new FileVO();
+
+//		file_path(project/userId/)
+//		file_id(추후 파일명 암호화 예정)
+//		file_nm(파일명)
+//		file_url(/homework/img/)
+//		order_no(auto increment)
+//		file_size(size)
+//		file_type(.jpg)
+//		fileDao.insert(fileVO);
+
+		String orgName = mfile.getOriginalFilename();
+		File target = new File(uploadPath, orgName);
+		FileCopyUtils.copy(mfile.getBytes(), target);
+
+		fvo.setFileNm(mfile.getOriginalFilename());
+		fvo.setFilePath(uploadPath);
+		fvo.setFileSize(mfile.getSize());
+		fvo.setFileType(mfile.getContentType());
+		fvo.setFileUrl("/homework/img/");
+		fvo.setOrderNo(0);
+
+		sqlSession.insert("_file.insert", fvo);
+		System.out.println(fvo.getFileSeq());
+
+		return fvo.getFileSeq();
 	}
 }
--- src/main/java/com/isaac/bcu/file/FileVO.java
+++ src/main/java/com/isaac/bcu/file/FileVO.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 @Data
 public class FileVO {
-
 	int fileSeq;
 	String filePath;
 	String fileId;
