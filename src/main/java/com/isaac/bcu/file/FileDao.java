c3ac05012ce388934cd60156b8eea1b91ea2c023
hitman1439 <isaac1102 hitman1439@naver.com>
2020-09-14 17:58:46 +0900

    다중 파일 업로드로 수정

--- src/main/java/com/isaac/bcu/file/FileDao.java
+++ src/main/java/com/isaac/bcu/file/FileDao.java
@@ -2,13 +2,16 @@ package com.isaac.bcu.file;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import org.springframework.util.FileCopyUtils;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.isaac.bcu.homework.HomeworkVO;
 
@@ -30,10 +33,6 @@ public class FileDao{
 		return sqlSession.selectOne("_homework.view", hwVO);
 	}
 
-	public int insert(MultipartFile mfile) throws IOException {
-
-		FileVO fvo = new FileVO();
-
 //		file_path(project/userId/)
 //		file_id(추후 파일명 암호화 예정)
 //		file_nm(파일명)
@@ -42,21 +41,48 @@ public class FileDao{
 //		file_size(size)
 //		file_type(.jpg)
 //		fileDao.insert(fileVO);
+	public List<Integer> insert(MultipartHttpServletRequest mRequest) throws IOException {
 
-		String orgName = mfile.getOriginalFilename();
-		File target = new File(uploadPath, orgName);
-		FileCopyUtils.copy(mfile.getBytes(), target);
+		List<MultipartFile> fileList = mRequest.getFiles("file");
 
-		fvo.setFileNm(mfile.getOriginalFilename());
-		fvo.setFilePath(uploadPath);
-		fvo.setFileSize(mfile.getSize());
-		fvo.setFileType(mfile.getContentType());
-		fvo.setFileUrl("/hwImg/img/");
-		fvo.setOrderNo(0);
+		int orderNo = 0;
+		List<Integer> fileSeqs = new ArrayList<Integer>();
+		for( MultipartFile  mfile : fileList ) {
+			FileVO fvo = new FileVO();
 
-		sqlSession.insert("_file.insert", fvo);
-		System.out.println(fvo.getFileSeq());
+			String orgName = mfile.getOriginalFilename();
+			File target = new File(uploadPath, orgName);
+			FileCopyUtils.copy(mfile.getBytes(), target);
 
-		return fvo.getFileSeq();
+			fvo.setFileNm(mfile.getOriginalFilename());
+			fvo.setFilePath(uploadPath);
+			fvo.setFileSize(mfile.getSize());
+			fvo.setFileType(mfile.getContentType());
+			fvo.setFileUrl("C:\\img");
+//			fvo.setFileUrl("/hwImg/img/");
+			fvo.setOrderNo(orderNo);
+			orderNo++;
+			int fileSeq = sqlSession.insert("_file.insert", fvo);
+			fileSeqs.add(fileSeq);
+		}
+
+
+//
+//		String orgName = mfile.getOriginalFilename();
+//		File target = new File(uploadPath, orgName);
+//		FileCopyUtils.copy(mfile.getBytes(), target);
+//
+//		fvo.setFileNm(mfile.getOriginalFilename());
+//		fvo.setFilePath(uploadPath);
+//		fvo.setFileSize(mfile.getSize());
+//		fvo.setFileType(mfile.getContentType());
+//		fvo.setFileUrl("/hwImg/img/");
+//		fvo.setOrderNo(0);
+
+//		sqlSession.insert("_file.insert", fvo);
+//		System.out.println(fvo.getFileSeq());
+
+		return fileSeqs;
+//		return fvo.getFileSeq();
 	}
 }
